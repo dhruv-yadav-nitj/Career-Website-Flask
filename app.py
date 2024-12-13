fbf1a0b8081aa01eb0f95d5a7f2b9db6b8022f03
Dhruv <dhruv.nitj@gmail.com>
2024-12-14 03:34:43 +0530

    added dynamic data config

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from livereload import Server
 
-app = Flask(__name__)
-@app.route('/')
+jobs = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Gurgaon, India'
+    },
+    {
+        'id': 2,
+        'title': 'Data Engineer',
+        'location': 'Bengaluru, India'
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'location': 'Hyderabad, India'
+    }
+]
 
-def hello_world():
-    return render_template('index.html')
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', job_data=jobs)
+
+
+@app.route('/api/jobs')
+def get_jobs():
+    return jsonify(jobs)
