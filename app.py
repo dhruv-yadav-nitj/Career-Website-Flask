1b31108b43a90172440bdca49838f1a50cafef6a
Dhruv <dhruv.nitj@gmail.com>
2024-12-14 05:41:26 +0530

    made visual changes

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ jobs = [
     {
         'id': 1,
         'title': 'Data Analyst',
-        'location': 'Gurgaon, India'
+        'location': 'Gurgaon, India',
+        'experience': '0-1 years'
     },
     {
         'id': 2,
         'title': 'Data Engineer',
-        'location': 'Bengaluru, India'
+        'location': 'Bengaluru, India',
+        'experience': '0-1 years'
     },
     {
         'id': 3,
         'title': 'Data Scientist',
-        'location': 'Hyderabad, India'
+        'location': 'Hyderabad, India',
+        'experience': '5-6 years'
     }
 ]
 
