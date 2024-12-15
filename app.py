ba697fad740680e64bffb637cb123bd49679d62a
Dhruv <dhruv.nitj@gmail.com>
2024-12-16 03:41:01 +0530

    added database data flow

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
 from flask import Flask, render_template, jsonify
 from livereload import Server
+from database import load_job_from_db
 
-jobs = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Gurgaon, India',
-        'experience': '0-1 years'
-    },
-    {
-        'id': 2,
-        'title': 'Data Engineer',
-        'location': 'Bengaluru, India',
-        'experience': '0-1 years'
-    },
-    {
-        'id': 3,
-        'title': 'Data Scientist',
-        'location': 'Hyderabad, India',
-        'experience': '5-6 years'
-    }
-]
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    jobs = load_job_from_db()
     return render_template('index.html', job_data=jobs)
 
 
 @app.route('/api/jobs')
 def get_jobs():
+    jobs = load_job_from_db()
     return jsonify(jobs)
---
+++ database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+conn_info = os.getenv("DATABASE_CONNECTION")
+
+engine = create_engine(conn_info)
+
+
+# --> testing purpose
+'''
+with engine.connect() as conn:
+    res = conn.execute(text("SELECT * FROM Jobs"))  # list of all the rows
+    all_res = res.all()
+
+    print(type(res), type(all_res), type(all_res[0]))
+
+    jobs = []
+    for row in all_res:
+        jobs.append(row._mapping)  # found this type conversion in the comments section -> was earlier using dict(row)
+    print(jobs)
+    
+'''
+
+def load_job_from_db():
+    with engine.connect() as conn:
+        res = conn.execute(text("SELECT * FROM Jobs"))  # list of all the rows
+        all_res = res.all()
+        jobs = []
+        for row in all_res:
+            jobs.append(row._mapping)  # found this type conversion in the comments section -> was earlier using dict(row)
+        return jobs
